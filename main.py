614b6041bd7c4ddbd65ce7d8e152e9b785c54e39
Bill Pottle <49958404+billpottle@users.noreply.github.com>
2021-02-13 19:20:44 -0700

    Make select boxes dynamic

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import pandas as pd
 from tkinter import filedialog
+from functools import partial
 
 root = Tk()
 root.geometry("400x400")
@@ -9,26 +10,41 @@ df1 = []
 df2 = []
 cols = []
 cols2 = []
+final_cols = []
+merged_cols = []
+
+
+def updateCols1():
+	col1_sel['values'] = cols 
+
+def updateCols2():
+	col2_sel['values'] = cols2 
+
+col1_sel = ttk.Combobox(root, value=cols, postcommand= updateCols1)
+col2_sel = ttk.Combobox(root, value=cols2, postcommand = updateCols2)
+
 
 def select_columns():
-	global cols, cols2
-	temp_cols = cols
-	temp_col2s = cols2
+	global cols, cols2, cols_sel, col2_sel
 	label = Label(root, text="Select columns from file 1 to merge with file 2")
 	label.pack()
-	col1_sel = ttk.Combobox(root, value=temp_cols)
-	col1_sel.current(0)
+	
 	col1_sel.pack(pady=10)
-	col2_sel = ttk.Combobox(root, value=temp_col2s)
-	col2_sel.current(0)
 	col2_sel.pack(pady=10)
+
 	merge_cols_button = Button(root, text="Merge Columns", command=merge_cols)
 	merge_cols_button.pack()
 	
 
 # Add file1_col to the final column list
 # Remove file1_col and file2_col from dropdowns
-def merge_cols(file1_col, file2_col): 
+def merge_cols(): 
+	global final_cols, cols, cols2
+	print(cols, col1_sel.get())
+	merged_cols.append((col1_sel.get(), col2_sel.get()))
+	cols.remove(col1_sel.get())
+	cols2.remove(col2_sel.get())
+	print(merged_cols)
 
 
 # Do the actual merging
@@ -54,6 +70,8 @@ def merge():
 	print('Final data frame', final_df)
 	final_df.to_csv('output.csv')
 # Create open dialog box function
+
+
 def open_file_1():
 	# Open File Dialog Box
 	global df1, cols
@@ -61,6 +79,7 @@ def open_file_1():
 	    initialdir='/guis', title="Open CSV File", filetypes = (("CSV Files","*.csv"),))
 	df1 = pd.read_csv(root.filename)
 	cols = list(df1.columns)
+	cols.insert(0, 'Column from File 1')
 	load_first_button.pack_forget()
 	check_show_begin()
 
@@ -71,6 +90,7 @@ def open_file_2():
 	    initialdir='/guis', title="Open CSV File", filetypes = (("CSV Files","*.csv"),))
 	df2 = pd.read_csv(root.filename)
 	cols2 = list(df2.columns)
+	cols2.insert(0, 'Column from File 2')
 	load_second_button.pack_forget()
 	check_show_begin()
 
