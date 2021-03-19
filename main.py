7dfcf8c6e3cf5129b609158dc129364faff7739a
Bill Pottle <49958404+billpottle@users.noreply.github.com>
2021-03-19 08:28:05 -0600

    Updating UI

--- main.py
+++ main.py
@@ -34,11 +34,10 @@ my_menu.add_cascade(label='Help', menu=help_menu)
 help_menu.add_command(label='About', command=new_merge)
 help_menu.add_command(label='Instructions', command=root.quit)
 
+
+
 #0 means take from first file, 1 means take from 2nd, 2 means merged
 final_cols_mapping = []
-
-
-
 
 def updateCols1():
 	col1_sel['values'] = cols 
@@ -50,23 +49,35 @@ col1_sel = ttk.Combobox(root, value=cols, postcommand= updateCols1)
 col2_sel = ttk.Combobox(root, value=cols2, postcommand = updateCols2)
 
 
+def choose_merge(): 
 
+	col1_sel.grid(row=10, column = 0, pady=10)
+	col2_sel.grid(row=10, column = 1, pady=10)
 
+	merge_cols_button = Button(root, text="Add Merge Columns", command=merge_cols)
+	merge_cols_button.grid(row=11, column = 0, pady=10)
+
+	done_cols_button.grid_forget()
+	filter_cols_button.grid_forget()
 
 
 def select_columns():
-	global cols, cols2, cols_sel, col2_sel
-	label = Label(root, text="Select columns from file 1 to merge with file 2")
-	label.pack()
+	global cols, cols2, cols_sel, col2_sel, done_cols_button, filter_cols_button
 	
-	col1_sel.pack(pady=10)
-	col2_sel.pack(pady=10)
+	col1_sel.grid(row=5, column = 0, pady=10)
+	col2_sel.grid(row=5, column = 1, pady=10)
 
-	merge_cols_button = Button(root, text="Add Merge Columns", command=merge_cols)
-	merge_cols_button.pack()
+	#col1_sel.grid(row=10, column = 0, pady=10)
+	#col2_sel.grid(row=10, column = 1, pady=10)
 
-	filter_cols_button = Button(root, text="Add Match Columns", command=match_cols)
-	filter_cols_button.pack()
+	#merge_cols_button = Button(root, text="Add Merge Columns", command=merge_cols)
+	#merge_cols_button.grid(row=5, column = 0, pady=10)
+
+
+	done_cols_button.grid(row=6,column=0, pady=10)
+
+	
+	filter_cols_button.grid(row=6, column = 1, pady=10)
 	
 
 # Add file1_col to the final column list
@@ -242,7 +253,7 @@ def rows_match(row1, row2):
 # Create open dialog box function
 def open_file_1():
 	# Open File Dialog Box
-	global df1, cols, original_cols
+	global df1, cols, original_cols, load_first_button
 	root.filename = filedialog.askopenfilename(
 	    initialdir='/guis', title="Open CSV File", filetypes = (("CSV Files","*.csv"),))
 	df1 = pd.read_csv(root.filename)
@@ -250,10 +261,14 @@ def open_file_1():
 	original_cols = list(df1.columns)
 	cols.insert(0, 'Column from File 1')
 	check_show_begin()
+	load_first_button.grid_forget()
+	first_file_label = Label(root, text=root.filename + ' loaded', fg= 'blue' )
+	first_file_label.grid(row=2, column = 0)
+
 
 def open_file_2():
 	# Open File Dialog Box
-	global df2, cols2, original_cols2
+	global df2, cols2, original_cols2, load_second_button
 	root.filename = filedialog.askopenfilename(
 	    initialdir='/guis', title="Open CSV File", filetypes = (("CSV Files","*.csv"),))
 	df2 = pd.read_csv(root.filename)
@@ -261,54 +276,61 @@ def open_file_2():
 	original_cols2 = list(df2.columns)
 	cols2.insert(0, 'Column from File 2')
 	check_show_begin()
-
-
-def show_select(options):
-	my_combo = ttk.Combobox(root, value=options)
-	my_combo.current(0)
-	my_combo.pack(pady=10)
+	load_second_button.grid_forget()
+	second_file_label = Label(root, text=root.filename + ' loaded', fg= 'red' )
+	second_file_label.grid(row=2, column = 1)
 
 
 def check_show_begin():
 	global df1, df2
 	if len(df1) > 0 and len(df2) > 0:
-		start_button = Button(root, text="Select Columns", command=select_columns)
-		start_button.pack()
+		select_columns()
 
 
 
 intro = Label(root, text='Welcome to CSV Merger. Follow the steps below or choose help in the menu for details')
-intro.grid(row=0, column= 0)
+intro.grid(row=0, column= 0, pady=10, columnspan=2)
 
 step_1 = Label(root, text="Step 1 - Choose 2 files to merge:", font=('Helvetica', 24))
-step_1.grid(row=1, column = 0)
+step_1.grid(row=1, column = 0, pady=10, columnspan=2)
 
 
 global load_first_button
-load_first_button = Button(root, text="Load First File", command=open_file_1)
-load_first_button.grid(row=2, column = 1)
-load_second_button = Button(root, text="Load Second File", command=open_file_2)
-load_second_button.grid(row=2, column = 3)
+load_first_button = Button(root, text="Load First File", bg = '#FFFFFF', command=open_file_1)
+load_first_button.grid(row=2, column = 0)
+load_second_button = Button(root, text="Load Second File",bg = '#FFFFFF', command=open_file_2)
+load_second_button.grid(row=2, column = 1)
 
-step_2 = Label(root, text="Step 2 - Choose Match and Merge Columns:", font=('Helvetica', 24))
-step_2.grid(row=3, column = 0)
+step_2 = Label(root, text="Step 2 - Choose Match Columns:", font=('Helvetica', 24))
+step_2.grid(row = 3, column = 0, pady=10, columnspan=2)
+step2_ins = Label(root, text="Match columns are columns where you want to check if both files have an equal value.")
+step2_ins.grid(row = 4, columnspan = 2)
 
-match_label = Label(root, text="Columns used to determine matched records")
-match_label.grid(row=4, column = 0)
+done_cols_button = Button(root, text="Match Columns Complete", command=choose_merge, fg = '#FFFFFF', bg = 'green')
+filter_cols_button = Button(root, text="Add Match Column", command=match_cols)
+
 match_fields_label = Label(root, text='Match columns = ')
-match_fields_label.grid(row=5, column = 0)
+match_fields_label.grid(row = 7, column = 0, columnspan = 2)
 
-merge_label = Label(root, text="Select columns from file 1 to merge with file 2")
-merge_label.grid(row=6, column = 0)
+
+step_3 = Label(root, text="Step 3 - Choose Merge Columns:", font=('Helvetica', 24))
+step_3.grid(row=8, column = 0, pady=10, columnspan=2)
+step3_ins = Label(root, text='Merge columns are those that you want to merge together regardless')
+step3_ins.grid(row=9, column = 0, pady=10)
+
+# row 1o is the selects
+# row 11 is the button
+
+merge_label = Label(root, text="Columns To be merged regardless")
+merge_label.grid(row = 12, column = 0)
 merge_fields_label = Label(root, text='Merge columns = ')
-merge_fields_label.grid(row=7, column = 0)
+merge_fields_label.grid(row = 13, column = 0)
 
-step_3 = Label(root, text="Step 3 - Finalize merge to output.csv:", font=('Helvetica', 24))
-step_3.grid(row=8, column = 0)
+step_3 = Label(root, text="Step 4 - Finalize merge to output.csv:", font=('Helvetica', 24))
+step_3.grid(row=14, column = 0, pady=10, columnspan=2)
 
-merge_button = Button(root, text="Complete Merge", command = merge)
-merge_buttonstep_2 = Label(root, text="Step 2 - Choose Match and Merge Columns:", font=('Helvetica', 24))
-step_2.grid(row=9, column = 0)
+merge_button = Button(root, text="Complete Merge", fg = '#FFFFFF', bg = 'green' , command = merge)
+merge_button.grid(row = 15, column = 0, columnspan = 2)
 
 # POPUP###############################
 from tkinter import messagebox
